3b5e13bb035801ca85a7ff9feb60fdc7f61fe235
Nandini Tulsian <nandinitulsian3105@gmail.com>
2026-04-01 12:05:15 +0530

    Fix database configuration

--- core/settings.py
+++ core/settings.py
@@ -57,11 +57,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR}/db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
