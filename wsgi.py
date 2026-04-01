4a861d054313eeec5598b3f665de457df3fac8a8
Nandini Tulsian <nandinitulsian3105@gmail.com>
2026-04-01 10:35:52 +0530

    Add Vercel configuration

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
